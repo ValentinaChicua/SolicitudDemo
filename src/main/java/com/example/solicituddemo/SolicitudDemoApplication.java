33904517373bb1955e38942b7dcb08b5935a7d66
Valentina Chicuasuque Rodriguez <chicuasuquevalentina@gmail.com>
2026-07-08 14:22:32 -0500

    init

---
+++ src/main/java/com/example/solicituddemo/SolicitudDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.solicituddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolicitudDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolicitudDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/solicituddemo/SolicitudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.solicituddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolicitudDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
